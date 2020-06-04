98500c14fbdc0700514357cd01b7fcbfbfc542b6
nothingexessive <65922029+nothingexessive@users.noreply.github.com>
2020-06-04 10:48:20 -0400

    Update liuj_hangman.py
    this is for stage 8 of the hangman course but I can't check it since i still have the message "You can't solve this stage because the previous one was not completed. Please complete the previous stage"

--- semi-week3/liuj_hangman.py
+++ semi-week3/liuj_hangman.py
@@ -1,18 +1,58 @@
 import random
-choosen = list(random.choice(['python', 'java', 'kotlin', 'javascript']))
-print('H A N G M A N\n')
-hidden = list('-' * len(choosen))
-j = 0
-while j < 8:
-    print(''.join(hidden))
-    guess = input('Input a letter: ')
-    if guess in choosen:
-        for i in range(len(choosen)):
-            if choosen[i] == guess:
-                hidden[i] = guess
-        print()
-    else:
-        print('No such letter in the word\n')
-    j += 1
-print("""Thanks for playing!
-We'll see how well you did in the next stage""")
+
+list1 = ['python', 'java', 'kotlin', 'javascript']
+index = random.randint(0, 3)
+word = list1[index]
+listWord = list(word)
+hyphen = "-"*len(word)
+alreadyGuessed = []
+listHyphens = list(hyphen[0:len(word)])
+loopContinue = True
+tries = 8
+print("""H A N G M A N\n""")
+userInput = input('Type "play" to play the game, "exit" to quit:')
+if userInput == 'play':
+   while(loopContinue):
+     print()
+     print("".join(listHyphens))
+     userInput = input(("Input a letter: "))
+     if userInput.islower():
+        if len(userInput) == 1:
+           if userInput not in alreadyGuessed:
+              if (userInput in listWord):
+                 replacement = listWord.index(userInput)
+                 i=1
+                 while i<listWord.count(userInput):
+                    listHyphens[replacement] = userInput
+                    listWord[replacement]=None
+                    replacement = listWord.index(userInput)
+                    listHyphens[replacement] = userInput
+                    listWord[replacement]=None
+                    i+=1
+                 if i == 1:
+                    listHyphens[replacement] = userInput
+                    listWord[replacement]=None
+                    i+=1
+                 alreadyGuessed.append(userInput)
+              else:
+                 tries -= 1
+                 print("No such letter in the word")
+           else:
+              print("You already typed this letter")
+        else:
+           print("You should input a single letter")
+     else:
+        print("It is not an ASCII lowercase letter")
+
+
+     if (tries==0):
+        loopContinue = False
+     if (word == "".join(listHyphens)):
+        loopContinue = False
+     if (tries==0):
+       loopContinue = False
+     if (word == "".join(listHyphens)):
+        loopContinue = False
+   print("\nThanks for playing!\nWe'll see how well you did in the next stage")
+
+
